9452b10af98472d6c88b0a429722b04202c04d09
sara <sara.ekmanMedieinstitutet@gmail.com>
2022-05-16 22:36:41 +0200

    done

---
+++ app.js
@@ -0,0 +1,51 @@
+let express = require('express');
+const app = express();
+let fs = require('fs');
+const port = 4000;
+let nanoId = require('nanoid');
+let cors = require('cors') 
+
+app.use(express.json());
+app.use(express.static('./public'));
+app.use(cors())
+
+app.get('/adress', (req, res) => {
+    let addresses = getJson('./addresses.json');
+    res.json(addresses);
+});
+
+app.post('/adress', (req, res) => {
+    let addresses = getJson('./addresses.json');
+
+    let foundA = addresses.find((adress) => {
+        return adress.AdressName == req.body.AdressName && adress.epost == req.body.epost && adress.number === req.body.number;
+    });
+
+    if (foundA) {
+        let foundIndex = addresses.findIndex((adress) => adress.id == foundA.id);
+        addresses.splice(foundIndex, 1);
+        addresses = [...addresses, {
+            ...foundA, foundAdress: true
+        }];
+        fs.writeFileSync('./addresses.json', JSON.stringify(addresses));
+        return res.json("You have already adress");
+    } else {
+        addresses = [...addresses, {
+            ...req.body, id: nanoId.nanoid(6), foundAdress: false
+        }];
+        fs.writeFileSync('./addresses.json', JSON.stringify(addresses));
+        return res.json('Now you create adress');
+    }
+});
+
+function getJson(url) {
+    let dataContent = [];
+    if (fs.existsSync(url)) {
+        dataContent = JSON.parse(fs.readFileSync(url));
+    }
+    return dataContent;
+}
+
+app.listen(port, () => {
+    console.log('server is now running on port ' + port);
+});
---
+++ public/main.js
@@ -0,0 +1,68 @@
+window.addEventListener('load', async () => {
+    let addresses = await makeReq('http://localhost:4000/adress', 'GET');
+    renderData(addresses);
+    console.log(addresses);
+});
+
+let form = document.querySelector('form');
+let adressContainer = document.querySelector('#adressContainer');
+
+document.querySelector('form button').addEventListener('click', async (e) => {
+    e.preventDefault();
+    try {
+        let body = {
+            adressName: form.adressName.value,
+            number: form.number.value,
+            epost: form.epost.value
+        };
+        let status = await makeReq('http://localhost:4000/adress', 'POST', body);
+        console.log(status);
+        console.log(body);
+        if (status === 'You have already adress') {
+            alert('Den adressen redan registrerad');
+        } else {
+            let addresses = await makeReq('http://localhost:4000/adress', 'GET');
+            console.log(addresses);
+            renderData(addresses);
+        }
+        form.adressName.value = '';
+        form.number.value = '';
+        form.epost.value = '';
+    } catch (err) { console.log("Error" + err); };
+});
+
+async function makeReq(url, method, body) {
+    try {
+        let response = await fetch(url, {
+            method,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        });
+        console.log(response);
+        // if (response.status != 200) {
+        //     throw response
+        // }
+        if (response.status == 404) {
+            throw new Error('NOt FOUND');
+        }
+        return await response.json();
+    } catch (err) {
+        throw err;
+        // console.log('error' + err); 
+    }
+}
+
+function renderData(addresses) {
+    adressContainer.innerHTML = '';
+
+    addresses.forEach(adress => {
+        adressContainer.innerHTML += `<div class="adressContent">
+        <h3>Aderessens namn: ${adress.adressName}</h3>
+        <h3>gatunummer: ${adress.number}</h3>
+        <h3>E-post: ${adress.epost}</h3>
+    </div>`;
+    });
+
+}
